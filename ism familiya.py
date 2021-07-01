39d0215e5463917964977c155cb627c71a73c174
Xusan77 <bakaevxusan@gmail.com>
2021-07-01 10:42:55 +0500

    men qoralama degan fayl qo'shdim

--- ism familiya.py
+++ ism familiya.py
@@ -1,3 +1,41 @@
-ism = input("Ismingiz nima=")
-yosh = "44"
-print("Assalom alaykum, mening ismim " + ism, "mening yoshim "+yosh,"da")
+# ism = input("Ismingiz nima=")
+# yosh = "44"
+# print("Assalom alaykum, mening ismim " + ism, "mening yoshim "+yosh,"da")
+# a=int(input("son kiriting->"))
+# birlar=a%10
+# unlar=a//10
+# s=birlar+unlar
+# print(f"s={s}")
+# a=8
+# b=9
+# d=a+b
+# print(f"Yig'indisi->{d}")
+# c=a*b
+# print(f"ko'paytmasi->{c}")
+# s=(d%10)*(c%10)
+# print(f"s={s}")
+# v0=int(input("v0="))
+# a=int(input("a="))
+# t=int(input("t="))
+# g=9.81
+# s=v0+(a*t**2)/2
+# print(f"s={s}")
+
+# mevalar = ["anor","olma","gilos"]
+# for meva in mevalar:
+#     for harf in meva:
+#         print(harf)
+# mashinalar=["nexa","tico","mersedes"]
+# for mashina in mashinalar:
+#     for harf in mashina:
+#         print(harf)
+# 2-masala.sonlar to'plami berilgan bu to'plamni o'sish tartibida saralang.
+#sonlar=[2,8,4,5,4,1,0,9]
+#4-masala  raqamlar to'plamida elementi nechchiga teng bo'lsa shu elementni shuncha chop eting
+sonlar.sort(reverse=False)
+numbers=[2,1,6,7]
+for index in range(len(sonlar))
+    for key in range
+
+
+
